9ebf6fa801a099e172f8236634563da2662c7f9c
Hina-naureen <hina.naureen0102@gmail.com>
2025-01-23 13:03:01 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['example.com', 'another-domain.com', 'public'], // Allowed image domains
+domains: ['example.com', 'another-domain.com', 'public.com'],
+
   },
 };
 
